92b45585a6fba6064ade2cd705e9712d8a8402c6
AhmedShab <ahmed.vuw@gmail.com>
2018-07-23 08:07:57 +0300

    setup socket io

--- server/server.js
+++ server/server.js
@@ -1,74 +1,16 @@
 
 
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const errorhandler = require('errorhandler');
-const isProduction = process.env.NODE_ENV === 'production';
-require('dotenv').config();
-const session = require('express-session');
-const crypto = require('crypto');
+const port = 8000;
+const io = require('socket.io')();
 
-// Create global server object
-const app = express();
-
-app.use(cors());
-app.use(session({
- secret: crypto.randomBytes(12).toString('hex'),
- resave: false,
- saveUninitialized: false,
-})
-)
-
-// Normal express config defaults
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-if (!isProduction) {
- app.use(errorhandler());
-}
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (!isProduction) {
- app.use(function (err, req, res, next) {
-  console.log(err.stack);
-
-  res.status(err.status || 500);
-
-  res.json({
-   'errors': {
-    message: err.message,
-    error: err
-   }
-  });
- });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
- res.status(err.status || 500);
- res.json({
-  'errors': {
-   message: err.message,
-   error: {}
-  }
+io.on('connection', client => {
+ client.on('subscribeToTimer', interval => {
+  console.log('client is subscribing to timer with interval ', interval);
+  setInterval(() => {
+   client.emit('timer', new Date());
+  }, interval);
  });
 });
 
-app.get('/', (req, res) => {
- res.send('simple server')
-})
-
-// finally, let's start our app...
-var server = app.listen(process.env.PORT || 8000, function () {
- console.log('Listening on port ' + server.address().port);
-});
+io.listen(port);
+console.log('Listening on port ' + port);
--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {R} from 'react-router-dom';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <label htmlFor="">Username</label>
+        <input type="text" name="" id="" />
+        <button>Enter</button>
       </div>
     );
   }
